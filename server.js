b603d9d8243cde649b6904147259496f477e42ca
Steve Emmerson <stephen.t.emmerson@gmail.com>
2015-12-04 21:31:36 -0600

    Update Todo Completed status

--- server.js
+++ server.js
@@ -13,15 +13,24 @@ app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=true
 app.get('/todos', function (req, res) {
-	res.json(todos);
+	var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.findWhere(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.findWhere(filteredTodos, {completed: false});
+	}
+
+	res.json(filteredTodos);
 });
 
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
+	var matchedTodo = _.where(todos, {id: todoId});
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
