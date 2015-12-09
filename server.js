0d0943a305837c256cc6bbd01178dcf527b3d369
Steve Emmerson <stephen.t.emmerson@gmail.com>
2015-12-08 22:27:33 -0600

    GET todos

--- server.js
+++ server.js
@@ -41,16 +41,18 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.where(todos, {
-		id: todoId
-	});
 
-	if (matchedTodo) {
-		res.json(matchedTodo);
-	} else {
-		res.status(404).send();
-	}
+	db.todo.findById(todoId).then(function (todo) {
+		if (!!todo) {
+			res.json(todo.toJSON());
+		} else {
+			res.status(404).send();
+		}
+	}, function (e) {
+		res.status(500).send();
+	});
 });
+
 
 // POST /todos
 app.post('/todos', function(req, res) {
