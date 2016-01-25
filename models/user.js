fe195d277209087b27f419db0fe91d2aaee343c6
Steve Emmerson <stephen.t.emmerson@gmail.com>
2016-01-24 22:36:39 -0600

    add auth token to login

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 var bcrypt = require('bcrypt');
 var _ = require('underscore');
+var cryptojs = require('crypto-js');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(sequelize, DataTypes) {
 	var user = sequelize.define('user', {
@@ -68,6 +70,24 @@ module.exports = function(sequelize, DataTypes) {
 			toPublicJSON: function() {
 				var json = this.toJSON();
 				return _.pick(json, 'email', 'id', 'createdAt', 'updatedAt');
+			},
+			generateToken: function (type) {
+				if (!_.isString(type)) {
+					return undefined;
+				}
+
+				try {
+					var stringData = JSON.stringify({id: this.get("id"), type: type});
+					var encryptedData = cryptojs.AES.encrypt(stringData, 'abc^!123').toString();
+					var token = jwt.sign({
+						token: encryptedData
+					}, 'qwerty780');
+
+					return token;
+				} catch (e) {
+					console.error(e);
+					return undefined;
+				}
 			}
 		}
 	});
